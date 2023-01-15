f252cef5d8bc53b312e80cbea471e640969ac51e
Alex Chu <kchu30@myseneca.ca>
2023-01-15 01:32:35 -0500

    reflection added

--- ws01p2/foodorder.h
+++ ws01p2/foodorder.h
@@ -1,3 +1,12 @@
+/******************************************************************************
+//                    OOP345NDD - WS01 @ 15 Jan 2023
+//Full Name  : Alex Chu
+//Student ID#: 153954219
+//Email      : kchu30@myseneca.ca
+//
+//I have done all the coding by myself and only copied the code
+//that my professor provided to complete my workshops and assdignments.
+******************************************************************************/
 #ifndef SDDS_FOODORDER_H_
 #define SDDS_FOODORDER_H_
 #include <iostream>
--- ws01p2/w1_p2.cpp
+++ ws01p2/w1_p2.cpp
@@ -1,3 +1,13 @@
+/******************************************************************************
+//                    OOP345NDD - WS01 @ 15 Jan 2023
+//Full Name  : Alex Chu
+//Student ID#: 153954219
+//Email      : kchu30@myseneca.ca
+//
+//I have done all the coding by myself and only copied the code
+//that my professor provided to complete my workshops and assdignments.
+******************************************************************************/
+
 // Workshop 1 - Linkage, Storage Duration, Namespaces, and OS Interface
 // Michael Huang - 2021/11/28
 
