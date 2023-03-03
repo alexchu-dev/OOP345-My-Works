d4b60599283ca496559cc7f33b1d833bda278a0f
Alex Chu <kchu30@myseneca.ca>
2023-03-03 11:40:50 -0500

    ws06 init 2

--- ws06/College.h
+++ ws06/College.h
@@ -1,6 +1,6 @@
 /******************************************************************************
 //                    OOP345NDD - WS06 @ 28 Feb 2023
-//Module:      Employee Header
+//Module:      College Header
 //Full Name  : Alex Chu
 //Student ID#: 153954219
 //Email      : kchu30@myseneca.ca
@@ -8,24 +8,17 @@
 //I have done all the coding by myself and only copied the code
 //that my professor provided to complete my workshops and assdignments.
 ******************************************************************************/
-#ifndef SDDS_EMPLOYEE_H
-#define SDDS_EMPLOYEE_H
+#ifndef SDDS_COLLEGE_H
+#define SDDS_COLLEGE_H
 #include "Person.h"
 namespace sdds
 {
-   class Employee :
+   class College :
       public Person
    {
-      std::string m_name{};
-      size_t m_age{};
-      std::string m_id{};
+
    public:
-      Employee(std::istream& is);
-      std::string status() const;
-      std::string name() const;
-      std::string age() const;
-      std::string id() const;
-      void display(std::ostream& os) const;
+
    };
 }
 
--- ws06/Employee.cpp
+++ ws06/Employee.cpp
@@ -15,8 +15,33 @@ using namespace std;
 namespace sdds {
    Employee::Employee(std::istream& is)
    {
-      string line;
-      getline(is, line, ',');
+      string temp;
+      getline(is, temp);
+      temp.erase(0, temp.find(",") + 1); // Assume Tag has been processed by Utilities already, here do a pass-by and erase
+      temp.erase(0, temp.find_first_not_of(" ")); // Erase leading spaces
+      m_name = temp.substr(0, temp.find(","));
+      m_name.erase(m_name.find_last_not_of(" ") + 1); // Erase trailing spaces
+      temp.erase(0, temp.find(",") + 1); // Erase the name and delimiter from temp
+      // Since age is an integer, if the data input is a string, it throws error
+      try {
+         m_age = stoi(temp);
+      }
+      catch (std::invalid_argument const& e)
+      {
+         cout << m_name << "++Invalid record!" << endl;
+      }
+      temp.erase(0, temp.find(",") + 1);
+      temp.erase(0, temp.find_first_not_of(" "));
+      try {
+         if (temp[0] == 'e' || temp[0] == 'E')
+            m_id = temp.erase(temp.find_last_not_of(" ") + 1);
+         else {
+            throw ("++Invalid record!");
+         }
+      }
+      catch (const char* e) {
+         cout << m_name << e << endl;
+      }
    }
    std::string Employee::status() const
    {
@@ -29,12 +54,30 @@ namespace sdds {
 
    std::string Employee::age() const
    {
-      return m_age;
+      return to_string(m_age);
    }
 
    std::string Employee::id() const
    {
       return m_id;
+   }
+
+   void Employee::display(std::ostream& os) const
+   {
+      os.setf(ios::left);
+      os << "| ";
+      os.width(10);
+      os << "Employee";
+      os << "| ";
+      os.width(10);
+      os << m_id;
+      os << "| ";
+      os.width(20);
+      os << m_name;
+      os << "| ";
+      os.width(3);
+      os << m_age;
+      os << "|";
    }
 
 }
